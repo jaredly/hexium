039f125b060d59ff93a1347f1b7ae75da6c6cc19
Jared Forsyth <jared@khanacademy.org>
2014-07-30 01:50:33 -0700

    moving things, changing to fit hexo

---
+++ helper/index.js
@@ -0,0 +1,71 @@
+
+var Handlebars = require('hexo-renderer-handlebars').handlebars
+var moment = require('moment')
+
+module.exports = {
+  date: function (date, options) {
+    if (arguments.length === 1) {
+      options = date
+      date = Date.now()
+    }
+    return moment(date).format(options.hash.format)
+  },
+  asset: function (name) {
+    return hexo.config.root + name
+  },
+  encode: function (item) {
+    return new Handlebars.SafeString(encodeURIComponent(item))
+  },
+  excerpt: function () {
+    return new Handlebars.SafeString(this.excerpt)
+  },
+  url: function (options) {
+    if (options.hash.absolute) {
+      return this.url
+    }
+    return this.path
+  },
+  tags: function (tags, options) {
+    if (arguments.length === 1) {
+      options = tags
+      tags = this.tags
+    }
+    options = options.hash
+    if (!tags || !tags.length) return
+    return new Handlebars.SafeString((options.prefix || '') + tags.map(function (tag) {
+      return '<a href="' + hexo.config.root + tag.path + '">' + tag.name + '</a>'
+    }).join(options.separator || ' '))
+  },
+  foreach: function (context, options) {
+    var ret = ''
+    context.each(function (item) {
+      var vals = {}
+      for (var name in item) {
+        vals[name] = item[name]
+      }
+      ret += options.fn(vals)
+      // ret += options.fn(item)
+    })
+    return ret;
+    /*
+    return context.map(function (item) {
+      return options.fn(item)
+    }).join('')
+    */
+  },
+  debug: function(optionalValue) {
+    console.log("Current Context");
+    console.log("====================");
+    console.log(this);
+  
+    if (optionalValue) {
+if ('function' === typeof optionalValue) {
+  optionalValue = optionalValue()
+}
+      console.log("Value");
+      console.log("====================");
+      console.log(optionalValue);
+    }
+  }
+}
+
